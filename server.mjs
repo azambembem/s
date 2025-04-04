2e44431a9eaac784eb058b0bf2b9d231f420d5a4
azambembem <azamjan980713@gmail.com>
2025-04-04 13:40:09 +0900

    first commit

---
+++ server.mjs
@@ -0,0 +1,32 @@
+import express from "express";
+import { config } from "dotenv";
+import morgan from "morgan";
+config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  throw new Error("ERROR:");
+});
+
+app.get("/test1", (req, res) => {
+  res.status(200).json("Welcome to the error handler");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
+  next();
+});
+
+// app.post("/test", (req, res) => {
+//   const { name, age } = req.body;
+//   res.send(`name: ${name}, age: ${age}`);
+// });
+
+const PORT = process.env.PORT || 6060;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
