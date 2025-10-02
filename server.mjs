78ff357ba0ba869ddf495515d210eb9817f4fcee
azambembem <azamjan980713@gmail.com>
2025-10-02 23:32:56 +0900

    mistake

--- server.mjs
+++ server.mjs
@@ -6,11 +6,11 @@ const app = express();
 app.use(express.json());
 
 app.get("/test1", (req, res) => {
-  res.status(200).json("test12");
+  res.status(200).json("test12";
 });
 
 const PORT = process.env.PORT || 6060;
 
-app.listen(PORT,  => {
+app.listen(PORT, () => {
   console.log(`Server is running on port1 ${PORT}`);
 });
