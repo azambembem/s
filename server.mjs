2a4ae355e24df771169b15ee62d339f95ed8542f
azambembem <azamjan980713@gmail.com>
2025-10-02 16:22:48 +0900

    delete all

--- server.mjs
+++ server.mjs
@@ -1,32 +1,12 @@
 import express from "express";
 import { config } from "dotenv";
-import morgan from "morgan";
 config();
 
 const app = express();
 app.use(express.json());
 
-app.get("/test", (req, res) => {
-  throw new Error("ERROR:");
-});
-
 app.get("/test1", (req, res) => {
-  res.status(200).json("Welcome to the error handler");
-});
-app.get("/test2", (req, res) => {
-  res.status(500).json("Welcome to the error handler");
-});
-app.get("/test3", (req, res) => {
-  res.status(404).json("Welcome to the error handler");
-});
-app.get("/test4", (req, res) => {
-  res.status(400).json("Welcome to the error handler");
-});
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send(err.message);
-  next();
+  res.status(200).json("test1");
 });
 
 const PORT = process.env.PORT || 6060;
