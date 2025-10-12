1e923c2baa187988412e653a961ca59cd3d6c118
azambembem <azamjan980713@gmail.com>
2025-10-12 20:25:52 +0900

    test

--- server.mjs
+++ server.mjs
@@ -5,8 +5,12 @@ config();
 const app = express();
 app.use(express.json());
 
-app.get("/test1" (req, res) => {
+app.get("/test1", (req, res) => {
   res.status(200).json("test12");
+});
+
+app.post("/test2", (req, res) => {
+  console.log("test3);
 });
 
 const PORT = process.env.PORT || 6060;
